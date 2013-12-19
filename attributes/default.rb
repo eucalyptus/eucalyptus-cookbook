ff6fce62d1eff08d187064842b79b8fb9f15a01e
Vic Iglesias <viglesiasce@gmail.com>
2013-12-19 01:51:28 -0800

    Set proper default for install-type

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 #### Install Info
-default["eucalyptus"]["install-type"] = "package"
+default["eucalyptus"]["install-type"] = "packages"
 default["eucalyptus"]["source-repo"] = "https://github.com/eucalyptus/eucalyptus.git"
 default["eucalyptus"]["source-branch"] = "testing"
 default["eucalyptus"]["eucalyptus-repo"] = "http://downloads.eucalyptus.com/software/eucalyptus/3.4/centos/6/x86_64/"
