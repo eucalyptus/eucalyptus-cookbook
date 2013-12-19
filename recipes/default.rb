90dd269bc88adc734dc5e9a166ffb78196104411
Vic Iglesias <viglesiasce@gmail.com>
2013-12-19 01:20:08 -0800

    Fix issue when epel is already installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,7 +30,7 @@ end
 
 remote_file "/tmp/epel-release.rpm" do
   source node["eucalyptus"]["epel-rpm"]
-  not_if "rpm -qa | grep -qx 'epel-release'"
+  not_if "rpm -qa | grep 'epel-release'"
 end
 
 remote_file "/tmp/elrepo-release.rpm" do
