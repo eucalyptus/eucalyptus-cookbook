f750b1c0f85b006c61c28293022e80a87e3312fc
Vic Iglesias <viglesiasce@gmail.com>
2014-01-06 20:29:02 -0800

    Version Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'viglesiasce@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures eucalyptus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 depends          'yum'
 depends          'ntp'
 depends          'selinux'
