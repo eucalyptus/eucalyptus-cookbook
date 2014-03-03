7e2167854ea57787af3015df3afa1b38a1ee962b
Vic Iglesias <viglesiasce@gmail.com>
2014-03-03 11:30:20 -0800

    Configure das

--- recipes/configure-storage.rb
+++ recipes/configure-storage.rb
@@ -11,9 +11,19 @@ clusters = node["eucalyptus"]["topology"]["clusters"]
 command_prefix = "source #{node['eucalyptus']['admin-cred-dir']}/eucarc && #{node['eucalyptus']['home-directory']}"
 modify_property = "#{command_prefix}/usr/sbin/euca-modify-property"
 clusters.each do |cluster, info|
+  ### Set backend
   execute "Set storage backend" do
      command "#{modify_property} -p #{cluster}.storage.blockstoragemanager=#{info["storage-backend"]} | grep #{info["storage-backend"]}"
      retries 10
      retry_delay 30
+  end
+  ### Configure backend
+  case info["storage-backend"]
+  when "das"  
+    execute "Set das device" do
+      command "#{modify_property} -p #{cluster}.storage.dasdevice=#{info["das-device"]} | grep #{info["das-device"]}"
+      retries 10
+      retry_delay 30
+    end
   end
 end
