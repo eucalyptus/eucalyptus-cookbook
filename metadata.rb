8c949ffd7cedc139d0ec99bcd4c082fd415fd510
Vic Iglesias <viglesiasce@gmail.com>
2014-03-18 19:44:27 -0700

    Fix license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'eucalyptus'
 maintainer       'Vic Iglesias'
 maintainer_email 'viglesiasce@gmail.com'
-license          'All rights reserved'
+license          'Apache 2'
 description      'Installs/Configures eucalyptus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
