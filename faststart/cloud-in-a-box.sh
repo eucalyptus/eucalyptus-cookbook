90a4060ec906aef48d245a65ca092b9ab174f1b1
gregdek <greg.dekoenigsberg@gmail.com>
2014-04-22 00:34:52 -0400

    Added lots of error checks to cloud-in-a-box

--- faststart/cloud-in-a-box.sh
+++ faststart/cloud-in-a-box.sh
@@ -1,89 +1,157 @@
-#!/bin/bash -xe
-# Retrieved from:
-# https://raw2.github.com/eucalyptus/eucadev/master/cloud_in_a_box.sh
+#!/bin/bash
 
-# This script is for installing cloud-in-a-box. Documentation coming soon.
+# Assumes:
+#   1. valid cookbooks in ./cookbooks directory;
+#   2. valid nuke.json runlist;
+#   3. valid ciab.json runlist.
 
-# TODO: make sure to check eth0 or em1 to pick the right interface for ciab.json
-# TODO: check to make sure that EL 6.5 is in /etc/redhat-config
-# TODO: make sure that virtualization is enabled
+###############################################################################
+# SECTION 1: PRECHECK.
+# 
+# Any immediately diagnosable condition that might prevent Euca from being
+# properly installed should be checked here.
+###############################################################################
 
-# Ignore the following bits; we're hardcoding in the ciab.json file locally for now.
-#if [ -z "$publicips" ];then
-    # public IPs aren't set; prompt for them.
-#    echo "Enter the available range of public IP addresses for your cloud, in the following format:"
-#    echo "  xxx.xxx.xxx.xxx-yyy.yyy.yyy.yyy"
-#    echo "  (Example: 192.168.1.100-192.168.1.199)"
-#    read -t 30 publicips
-#    if [ "$?" -gt 0 ] ; then
-#        echo "Timeout waiting for IP address range"
-#        exit 1
-#    fi
-#else
-#    echo "Using Public IPs: $publicips"
-#fi
+echo ""
+echo ""
 
-# Quick and dirty hack to install new euca2ools client needed by 4.0.0
-# TODO: set euca2ools repo... in which recipe? 
-#   See: https://github.com/eucalyptus/eucadev/blob/master/Vagrantfile#L21
-# rpm -Uvh http://downloads.eucalyptus.com/software/euca2ools/nightly/3.1/centos/6/i386/python-requestbuilder-0.2.0-0.3.pre1.el6.noarch.rpm
-# rpm -Uvh http://downloads.eucalyptus.com/software/euca2ools/nightly/3.1/centos/6/i386/euca2ools-3.1.0-0.0.164.20140321git697a0826.el6.noarch.rpm 
+# Check to make sure I'm root
+echo "[Precheck] Checking root"
+ciab_user=`whoami`
+if [ "$ciab_user" != 'root' ]; then
+    echo "[FATAL] Not running as root"
+    echo ""
+    echo "Please run Eucalyptus Faststart as the root user."
+    exit 5
+fi
+echo "[Precheck] OK, running as root"
+echo ""
+echo ""
 
-# remove old Chef recipes
+# Check to see that we're running on CentOS or RHEL 6.5.
+echo "[Precheck] Checking OS"
+grep "6.5" /etc/redhat-release 
+if [ "$?" != "0" ]; then
+    echo "======"
+    echo "[FATAL] Operating system not supported"
+    echo ""
+    echo "Please note: Eucalyptus Faststart only runs on RHEL or CentOS 6.5."
+    echo "To try Faststart on another platform, consider trying Eucadev:"
+    echo "https://github.com/eucalyptus/eucadev"
+    echo ""
+    echo ""
+    wget -q https://www.eucalyptus.com/docs/tipoftheday.html?fserror=OS_NOT_SUPPORTED -O /dev/null
+    exit 10
+fi
+echo "[Precheck] OK, OS is supported"
+echo ""
+echo ""
+
+# Check to see if kvm is supported by the hardware.
+echo "[Precheck] Checking hardware virtualization"
+egrep '^flags.*(vmx|svm)' /proc/cpuinfo
+if [ "$?" != "0" ]; then
+    echo "====="
+    echo "[FATAL] Processor doesn't support virtualization"
+    echo ""
+    echo "Your processor doesn't appear to support virtualization."
+    echo "Eucalyptus requires virtualization to be enabled on your system."
+    echo "Please check your BIOS settings, or install Eucalyptus on a"
+    echo "system that supports virtualization."
+    echo ""
+    echo ""
+    wget -q https://www.eucalyptus.com/docs/tipoftheday.html?fserror=VIRT_NOT_SUPPORTED -O /dev/null
+    exit 20
+fi
+echo "[Precheck] OK, processor supports virtualization"
+echo ""
+echo ""
+
+echo "[Precheck] Precheck successful."
+echo ""
+echo ""
+
+###############################################################################
+# SECTION 2: USER INPUT
+#
+# For now, we're going to harcode the variables to make sure that the 
+# template replacement works properly.
+#
+# TODO: setup an interactive mode that asks all the necessary questions
+# TODO: setup an automated mode that reads the ciab.json file directly
+###############################################################################
+
+# Set the IP addresses.
+ciab_ipaddr="192.168.1.160"
+ciab_netmask="255.255.255.0"
+ciab_gateway="192.168.1.1"
+ciab_subnet="192.168.1.0"
+ciab_publicips1="192.168.1.161"
+ciab_publicips2="192.168.1.170"
+ciab_privateips1="192.168.1.171"
+ciab_privateips2="192.168.1.180"
+
+# Copy the CIAB template over to be the active CIAB configuration file.
+cp -f ciab-template.json ciab.json 
+
+# Perform variable interpolation in the CIAB template.
+sed -i "s/IPADDR/$ciab_ipaddr/g" ciab.json
+sed -i "s/NETMASK/$ciab_netmask/g" ciab.json
+sed -i "s/GATEWAY/$ciab_gateway/g" ciab.json
+sed -i "s/SUBNET/$ciab_subnet/g" ciab.json
+sed -i "s/PUBLICIPS1/$ciab_publicips1/g" ciab.json
+sed -i "s/PUBLICIPS2/$ciab_publicips2/g" ciab.json
+sed -i "s/PRIVATEIPS1/$ciab_privateips1/g" ciab.json
+sed -i "s/PRIVATEIPS2/$ciab_privateips2/g" ciab.json
+
+###############################################################################
+# SECTION 3: PREP THE INSTALLATION
+#
+# TODO: Add a confirm for interactive mode: "this will blow away any
+# current Eucalyptus installation on this machine, are you sure?"
+###############################################################################
+
+echo "[Prep] Removing old Chef templates"
+# Get rid of old Chef stuff lying about.
 rm -rf /var/chef/*
 
-echo "Installing Chef"
-curl -L https://www.opscode.com/chef/install.sh | bash > chef-install.log
+echo "[Prep] Tarring up cookbooks"
+# Tar up the cookbooks for use by chef-solo.
+tar czvf cookbooks.tgz cookbooks
 
-### Download artifacts
-if [ -z "$role" ];then
-    export role=ciab
+echo "[Prep] Nuking any previous Euca installation"
+# Run the nuke recipe, which gets rid of all traces of Euca.
+chef-solo -r cookbooks.tgz -j nuke.json 1>/tmp/ciab.nuke.out
+
+###############################################################################
+# SECTION 4: INSTALL EUCALYPTUS
+#
+# TODO: Add successful run time
+# TODO: Add an error parser to pull and report any FATAL chef error
+# TODO: urlencode and send error message upstream
+###############################################################################
+
+# Install Euca and start it up in the cloud-in-a-box configuration.
+echo ""
+echo ""
+echo "[Installing Eucalyptus]"
+echo ""
+echo "Please note: this installation will take a while. Go grab a cup of coffee."
+echo "If you want to watch the progress of this installation, you can check the"
+echo "log file by running the following command in another terminal:"
+echo ""
+echo "tail -f /tmp/ciab.install.out"
+
+chef-solo -r cookbooks.tgz -j ciab.json 1>/tmp/ciab.install.out
+
+if [ "$?" != "0" ]; then
+    echo "[FATAL] Eucalyptus installation failed"
+    echo ""
+    echo "Eucalyptus installation failed. Please consult the file /tmp/ciab.install.out for details."
+    exit 99
 fi
-echo "Using Role: $role"
 
-# OK, we really should be pulling the latest recipes from 4.0 every time.
-# Thus, we'll go ahead and pull these, and then rebuild the cookbooks file
-# so we're getting the latest cookbook every time.
-
-curl http://euca-chef.s3.amazonaws.com/cookbooks.tgz > COOKBOOK-DIRS/cookbooks.tgz
-cd COOKBOOK-DIRS
-wget https://github.com/eucalyptus/eucalyptus-cookbook/archive/master.zip 
-mv master.zip euca-cookbook.zip
-unzip -o euca-cookbook.zip
-gunzip cookbooks.tgz
-tar -xvf cookbooks.tar
-rm -rf cookbooks/old-eucalyptus
-mv cookbooks/eucalyptus cookbooks/old-eucalyptus
-mv eucalyptus-cookbook-master cookbooks/eucalyptus
-tar -cvf new-cookbooks.tar cookbooks
-gzip new-cookbooks.tar
-mv new-cookbooks.tar.gz ../new-cookbooks.tgz
-cd ..
-
-pwd
-
-# Comment the next line out and replace it with the line after; we're changing
-# the CIAB role to use 4.0 packages instead of 3.4.
-# TODO: replace with the right CIAB.json file when it's done
-# curl http://euca-chef.s3.amazonaws.com/ciab.json > $role.json
-
-# For now, we're not even downloading; we're just hacking the local copy and using it.
-#curl https://raw.githubusercontent.com/gregdek/fasterstart/master/ciab.json > $role.json
-#sed -i "s/PUBLICIPS/$publicips/g" $role.json
-#if [ ! -z "$bridge" ];then
-#    sed -i "s/eth0/$bridge/g" $role.json
-#fi
-
-echo "Triple checking the right json file:"
-cat $role.json
-
-exit
-
-# Comment out the next line using cookbooks.tgz and use the new-cookbooks.tgz instead.
-# chef-solo -r cookbooks.tgz -j $role.json
-chef-solo -r new-cookbooks.tgz -j $role.json 1>/tmp/chef-solo.out 2>/tmp/chef-solo.err
-# chef-solo -j $role.json 
-# chef-solo -j $role.json 1>/tmp/chef-solo.out 2>/tmp/chef-solo.err
-
-# Add tip of the day to Eucalyptus console
-# export UUID=`uuidgen` && sed -i "s|</body>|<iframe width=\"0\" height=\"0\" src=\"https://www.eucalyptus.com/docs/tipoftheday.html?${UUID}\" seamless=\"seamless\" frameborder=\"0\"></iframe></body>|" /usr/share/eucalyptus-console/static/index.html
+echo ""
+echo ""
+echo "[SUCCESS] Eucalyptus installation complete!"
+exit 0
