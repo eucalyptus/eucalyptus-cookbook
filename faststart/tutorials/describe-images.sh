d7354c6f8346d27cdf2e194802011c1e57342657
gregdek <greg.dekoenigsberg@gmail.com>
2014-05-23 18:04:37 -0400

    Too many changes in one commit, sigh. But in general: improving explanations, putting the tutorial image in the tutorial bucket, adding error checking, and so on.

--- faststart/tutorials/describe-images.sh
+++ faststart/tutorials/describe-images.sh
@@ -3,19 +3,22 @@
 bold=`tput bold`
 normal=`tput sgr0`
 
-# Skip code
-if false; then
-
 echo ""
 echo ""
-echo "${bold}A Brief Image Tutorial${normal}"
+echo "${bold}Listing Images${normal}"
 echo ""
-echo "Hit Enter to continue."
+echo "Continue with Listing Images Tutorial? (Y/n)"
 
 read continue
+if [ "$continue" = "n" ] || [ "$continue" = "N" ]
+then
+    echo "OK. To run though this tutorial at any time, run the following command: "
+    echo "  $0"
+    exit 1
+fi
 
-echo "The fundamental building block of Eucalyptus is the image,"
-echo "and we use euca2ools to interact with those images."
+echo "The fundamental building block of Eucalyptus is the image."
+echo "We use a program called euca2ools to interact with those images."
 echo ""
 echo "In this tutorial, we're going to show you how to list"
 echo "the images available to your Eucalyptus users."
@@ -27,18 +30,14 @@ read continue
 echo "Remember: when using Eucalyptus, you must \"log in\"."
 echo "When using euca2ools, the way to \"log in\" is to source"
 echo "the euca2ools credentials file. By default, Faststart"
-echo "installs your credentials fule in the root directory."
+echo "installs your credentials file in the root directory."
 echo ""
 echo "Hit Enter to run the command:"
 echo "  ${bold}source /root/eucarc${normal}"
 
 read continue
+
 echo "${bold}+ source /root/eucarc${normal}"
-
-
-# Unskip code
-fi
-
 source /root/eucarc
 
 echo "The euca2ools command for listing images is ${bold}euca-describe-images${normal}."
@@ -48,6 +47,7 @@ echo "nearly identical to the comparable AWS command; this is by design."
 echo "Press Enter to run ${bold}euca-describe-images${normal} now."
 
 read continue
+
 echo "${bold}+ euca-describe-images"
 euca-describe-images
 echo "${normal}"
@@ -70,4 +70,3 @@ echo ""
 
 echo "To learn more about the euca-describe-images command, check out the documentaion:"
 echo "  https://www.eucalyptus.com/docs/eucalyptus/3.4/index.html#euca2ools-guide/euca-describe-images.html"
-
--- faststart/tutorials/master-tutorial.sh
+++ faststart/tutorials/master-tutorial.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+bold=`tput bold`
+normal=`tput sgr0`
+
+echo ""
+echo "*****"
+echo ""
+echo "Welcome to the Getting Started Tutorial.  We will walk you through"
+echo "some of the key concepts of managing your new Eucalyptus cloud."
+echo "It is ${bold}strongly recommended${normal} for first-time users of Eucalyptus."
+echo ""
+echo "Would you like to walk through the Getting Started tutorial? [Y/n]"
+read continue
+if [ "$continue" = "n" ] || [ "$continue" = "N" ]
+then
+    echo "OK. To run though the full tutorial at any time, run the following command: "
+    echo "  $0"
+    echo ""
+    echo "You may also choose to run any of the individual tutorial scripts"
+    echo "in the tutorial/ directory at any time."
+    exit 1
+fi
+
 ./describe-images.sh
 ./install-image.sh
