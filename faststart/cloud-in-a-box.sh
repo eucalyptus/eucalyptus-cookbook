3bd3aca378912762aae34d74879ba34511d3717b
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2014-06-11 11:01:11 -0400

    Fix type

--- faststart/cloud-in-a-box.sh
+++ faststart/cloud-in-a-box.sh
@@ -258,7 +258,7 @@ if [ "$?" == "0" ]; then
     echo ""
     echo "The presence of NetworkManager indicates that you have installed a Desktop environment."
     echo "Please run Faststart on a minimal OS without a Desktop environment installed."
-    curl --silent "https://www.eucalyptus.com/docs/faststart_errors.html?msg=DESKTOP_NOT_SUPPORTEDi&id=$uuid" >> /tmp/fsout.log
+    curl --silent "https://www.eucalyptus.com/docs/faststart_errors.html?msg=DESKTOP_NOT_SUPPORTED&id=$uuid" >> /tmp/fsout.log
     exit 12
 fi
 
