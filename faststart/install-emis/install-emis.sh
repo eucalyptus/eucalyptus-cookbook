c0c8f15728fc948c60c6892dea1573fe225351f1
Vic Iglesias <viglesiasce@gmail.com>
2014-06-18 14:38:15 -0700

    Fix install-emis shell url

--- faststart/install-emis/install-emis.sh
+++ faststart/install-emis/install-emis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python <(curl -sL https://raw.githubusercontent.com/eucalyptus/eucalyptus-cookbook/master/faststart/fast-emis/fast-emis.py)
+python <(curl -sL https://raw.githubusercontent.com/eucalyptus/eucalyptus-cookbook/master/faststart/install-emis/install-emis.py)
