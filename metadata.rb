f6055063a6a2a4c271294674c12ca16709751bf6
Tony Beckham <tony.beckham@gmail.com>
2014-09-10 16:43:27 -0700

    Version bump to 0.3.15

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'viglesiasce@gmail.com'
 license          'Apache 2'
 description      'Installs/Configures eucalyptus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.14'
+version          '0.3.15'
 depends          'yum'
 depends          'ntp'
 depends          'selinux'
