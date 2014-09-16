bad0d3a7184216d748cb8a3b1dbf90d065a8fe7c
shaon <mdshaonimran@gmail.com>
2014-09-17 01:47:58 +0600

    add stack order and component group for ceph to motherbrain

--- motherbrain.rb
+++ motherbrain.rb
@@ -22,6 +22,11 @@ stack_order do
   bootstrap 'cloud::configure'
   bootstrap 'cloud::create-first-resources'
   bootstrap 'nuke::default'
+  bootstrap 'ceph::all-in-one'
+  bootstrap 'ceph::setup-mons'
+  bootstrap 'ceph::setup-osds'
+  bootstrap 'ceph::setup-admin'
+  bootstrap 'ceph::setup-mds'
 end
 
 component 'cloud' do
@@ -91,5 +96,34 @@ component 'nuke' do
   versioned
   group 'default' do
     recipe 'eucalyptus::nuke'
+  end
+end
+
+component 'ceph' do
+  description "ceph cookbook application"
+
+  group 'all-in-one' do
+    recipe 'ceph-cookbook::default'
+    recipe 'ceph-cookbook::mons'
+    recipe 'ceph-cookbook::osds'
+    recipe 'ceph-cookbook::admin'
+    recipe 'ceph-cookbook::mds'
+  end
+
+  group 'setup-osds' do
+    recipe 'ceph-cookbook::osds'
+  end
+
+  group 'setup-mons' do
+    recipe 'ceph-cookbook::default'
+    recipe 'ceph-cookbook::mons'
+  end
+
+  group 'setup-admin' do
+    recipe 'ceph-cookbook::admin'
+  end
+
+  group 'setup-mds' do
+    recipe 'ceph-cookbook::mds'
   end
 end
