d17169090a6eb46c9af451d226bf0cefaa93f336
Vic Iglesias <viglesiasce@gmail.com>
2015-04-20 14:06:34 -0700

    Dont install SAN packages when source or cluster is not SAN backed

--- recipes/storage-controller.rb
+++ recipes/storage-controller.rb
@@ -72,19 +72,26 @@ if Eucalyptus::Enterprise.is_san?(node)
       san_package = 'eucalyptus-enterprise-storage-san-netapp'
     when 'equallogic'
       san_package = 'eucalyptus-enterprise-storage-san-equallogic'
+    else
+      # This cluster is not SAN backed
+      san_package = nil
     end
-    yum_package san_package do
-      action :upgrade
-      options node['eucalyptus']['yum-options']
-      notifies :restart, "service[eucalyptus-cloud]", :immediately
-      flush_cache [:before]
-    end
-    if Eucalyptus::Enterprise.is_vmware?(node)
-      yum_package 'eucalyptus-enterprise-vmware-broker-libs' do
-        action :upgrade
-        options node['eucalyptus']['yum-options']
-        notifies :restart, "service[eucalyptus-cloud]", :immediately
-        flush_cache [:before]
+    if node["eucalyptus"]["install-type"] == "packages"
+      if san_package
+        yum_package san_package do
+          action :upgrade
+          options node['eucalyptus']['yum-options']
+          notifies :restart, "service[eucalyptus-cloud]", :immediately
+          flush_cache [:before]
+        end
+      end
+      if Eucalyptus::Enterprise.is_vmware?(node)
+        yum_package 'eucalyptus-enterprise-vmware-broker-libs' do
+          action :upgrade
+          options node['eucalyptus']['yum-options']
+          notifies :restart, "service[eucalyptus-cloud]", :immediately
+          flush_cache [:before]
+        end
       end
     end
   end
