9dcd729436650ebed7fbb1b15bb1c10c70145b62
Vasiliy Kochergin <vasya@eucalyptus.com>
2015-04-24 12:58:34 -0700

    Start imaing worker stack creation

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -171,6 +171,9 @@ if node['eucalyptus']['install-service-image']
   execute "source #{node['eucalyptus']['admin-cred-dir']}/eucarc && export EUCALYPTUS=#{node["eucalyptus"]["home-directory"]} && esi-install-image --install-default" do
     only_if "#{describe_property} services.imaging.worker.image | grep 'NULL'"
   end
+  execute "#{modify_property} -p services.imaging.worker.configured=true" do
+    only_if "#{describe_property} services.imaging.worker.configured | grep 'false'"
+  end
 end
 
 node['eucalyptus']['system-properties'].each do |key, value|
