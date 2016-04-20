be373e2ada1f6f5c7c9305c6ee25e77f38eb9e2a
Imran Hossain Shaon <mdshaonimran@gmail.com>
2016-04-20 13:37:46 -0700

    update - less verbose, add default user

--- recipes/setupcreds.rb
+++ recipes/setupcreds.rb
@@ -3,35 +3,43 @@ directory '/root/.euca' do
   group 'root'
   mode '0755'
   action :create
+  ignore_failure true
 end
 
 bash 'create_admin_creds' do
   cwd ::File.dirname('/root')
   code <<-EOH
     eval `clcadmin-assume-system-credentials`
-    dnsdomain=$(euctl | grep dnsdomain | awk '{print $3}')
-    euare-useraddkey admin -wd $dnsdomain > /root/.euca/euca-admin.ini
-    echo "[global]" >> /root/.euca/euca-admin.ini; echo "default-region = $dnsdomain" >> /root/.euca/euca-admin.ini
-    euare-useraddloginprofile admin -p foobar --region admin@
+    DNSDOMAIN=$(euctl | grep dnsdomain | awk '{print $3}')
+    euare-useraddkey admin -wd $DNSDOMAIN > /root/.euca/euca-admin.ini
+    ACCOUNTID=$(grep account-id /root/.euca/euca-admin.ini | awk '{print $3}')
+    grep "user = $ACCOUNTID:admin" /root/.euca/euca-admin.ini
+    if [ $? -ne 0 ]; then
+      sed -i "/\\[region/auser = $ACCOUNTID:admin" /root/.euca/euca-admin.ini
+      echo "[global]" >> /root/.euca/euca-admin.ini; echo "default-region = $DNSDOMAIN" >> /root/.euca/euca-admin.ini
+    fi
+    #euare-useraddloginprofile admin -p foobar --region admin@
 
     cat > /etc/motd << EOF
 
 ------------------------------------------------------------
-    ** Eucalyptus Admin credentials:
-    `grep user /root/.euca/euca-admin.ini`
-        `grep key-id /root/.euca/euca-admin.ini`
-        `grep secret-key /root/.euca/euca-admin.ini`
-        `grep account-id /root/.euca/euca-admin.ini`
 
-    ** admin example:
-        euserv-describe-services --region admin@
+    * Defauls region is set to $DNSDOMAIN
+    * Default user credentials:
+      * account: eucalyptus ($ACCOUNTID)
+      * user: arn:aws:iam::$ACCOUNTID:user/admin
+
+    * example command as eucalyptus admin:
+      * euserv-describe-services
 
     ** Login Profile: (requires eucaconsole)
         account: eucalyptus
         username: admin
         password: foobar
+
 ------------------------------------------------------------
 
 EOF
     EOH
+  ignore_failure true
 end
