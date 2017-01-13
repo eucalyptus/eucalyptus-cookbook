c3b07d2726b8995bc9b9cdabcc7c269f4f179b11
Matt Bacchi <mbacchi@hpe.com>
2017-01-13 11:57:42 -0500

    Ignore rc=1 from systemd-modules-load command in node-controller recipe

--- recipes/node-controller.rb
+++ recipes/node-controller.rb
@@ -167,7 +167,7 @@ end
 ## use a different notifier to setup bridge in VPCMIDO mode
 if node["eucalyptus"]["network"]["mode"] != "VPCMIDO"
   execute "Ensure bridge modules loaded into the kernel on NC" do
-    command '/usr/lib/systemd/systemd-modules-load'
+    command '/usr/lib/systemd/systemd-modules-load || :'
     notifies :run, "execute[network-restart]", :immediately
     notifies :run, "execute[brctl setfd]", :delayed
     notifies :run, "execute[brctl sethello]", :delayed
@@ -175,7 +175,7 @@ if node["eucalyptus"]["network"]["mode"] != "VPCMIDO"
   end
 else
   execute "Ensure bridge modules loaded into the kernel on NC" do
-    command '/usr/lib/systemd/systemd-modules-load'
+    command '/usr/lib/systemd/systemd-modules-load || :'
     notifies :run, "execute[ifup-br0]", :immediately
   end
 end
